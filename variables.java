622e2a1dfa6bd0c3ae89457b03f1ba8690cc1bca
sandeep-pilli06 <sandeeppilli06@gmail.com>
2025-02-12 22:40:29 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {  
+  int a = 9; // 
+
+  public static void main(String[] args) {
+    variables obj = new variables();
+    System.out.println("Hello, Khagesh");
+    System.out.println("Output: " + obj.a);
+  }
+}
