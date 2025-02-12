f828b41f05bffca418c1ae103225a80c05a26b19
sandeep-pilli06 <sandeeppilli06@gmail.com>
2025-02-12 22:42:59 +0530

    Add files via upload

---
+++ loops/loop1.java
@@ -0,0 +1,11 @@
+//program of for loop
+public class loop1
+{
+  public static void main(String[] args)
+  {
+    for(int i=0;i<5;i++)
+    {
+      System.out.println(i);
+    }
+  }
+}
---
+++ loops/loop3.java
@@ -0,0 +1,14 @@
+//program of do-while loop
+public class loop3
+{
+  public static void main(String[] args)
+  {  
+    int i=0;
+    do
+    {
+      System.out.println(i);
+        i++;
+    }
+    while(i<5);
+  }
+}
